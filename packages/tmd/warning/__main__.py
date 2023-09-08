1bdb7ef2bffe84cdbc486f82155b6761a35c21d9
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-09-08 08:48:37 +0700

    remove/improve: Remove Alert Affected To

--- packages/tmd/warning/__main__.py
+++ packages/tmd/warning/__main__.py
@@ -32,7 +32,6 @@ def get_tmd_weather(get_latest: bool):
       formatter = {
           "alert_at":"",
           "alert_no":"",
-          "alert_affected_to":"",
           "details":"",
           "alert_title":"",
           "alert_details":"",
@@ -54,7 +53,6 @@ def get_tmd_weather(get_latest: bool):
       formatter['details'] = details.replace("  "," ")
       formatter['alert_title'] = head_title
       formatter['alert_details'] = head_details
-      formatter['alert_affected_to'] = thai_strptime(' '.join(re.findall(r'\([^)]*\)', formatter['details'])[-1].split(" ")[1:]).replace(")",""),"%d %B %Y").isoformat()
       formatter['alert_no'] = re.findall(r'ฉบับที่\s+[0-9]+', formatter['details'])[-1].replace("ฉบับที่ ",'')
       for content in classify_content:
         if (content.find("img")):
