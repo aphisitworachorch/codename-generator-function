8787118067519473368d7e1fc064c87fb1c13f99
Arsanandha Aphisitworachorch <arsanandha.ap@gmail.com>
2024-08-18 15:50:33 +0700

    fix: Fix Station

--- packages/codename/mrta_generator/__main__.py
+++ packages/codename/mrta_generator/__main__.py
@@ -5,6 +5,6 @@ def main():
   try:
     p = BangsueCodename.MRTATrain()
     codename = p.get_code_name()
-    return {"body": p.convert_codename_to_string(codename, "station_with_number"),"status": HTTPStatus.OK}
+    return {"body": p.convert_codename_to_string(codename, "all"),"status": HTTPStatus.OK}
   except Exception as e:
     return {"body": e,"status": HTTPStatus.INTERNAL_SERVER_ERROR}
